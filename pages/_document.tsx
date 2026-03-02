0f2f14698c2b64c53cc6a768ccea575d0259da98
Abdulaziz Abdukhalimov <abdukhalimov_front_end@Abdulazizs-MacBook-Pro.local>
2026-03-02 15:36:35 +0900

    fix: final link meta corrections and input text area correction

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,10 +12,10 @@ export default function Document() {
 				<meta
 					name={'description'}
 					content={
-						'Buy and rent yachts, jetskis, boats anywhere anytime Internationally. Live in your luxury dream life. Best Services and Events at Best prices on oceancraft.com | ' +
-						'Покупайте и арендуйте яхты, гидроциклы, лодки в любое время и в любом месте по всему миру. Живите роскошной жизнью своей мечты. Лучшие услуги и мероприятия по лучшим ценам на oceancraft.com | ' +
-						'요트, 제트스키, 보트를 언제 어디서든 전 세계에서 구매하고 렌트하세요. 꿈꿔왔던 럭셔리한 삶을 누려보세요. oceancraft.com에서 최고의 서비스와 이벤트를 최저가로 만나보세요. | ' +
-						'Xalqaro miqyosda istalgan vaqtda istalgan joyda yaxtalar, jetskilar, qayiqlarni sotib oling va ijaraga oling. Hashamatli orzuingizdagi hayotda yashang. Oceancraft.com saytida eng yaxshi narxlarda eng yaxshi xizmatlar va tadbirlar.'
+						'Buy and rent yachts, jetskis, boats anywhere anytime Internationally. Live in your luxury dream life. Best Services and Events at Best prices on Oceancraft | ' +
+						'Покупайте и арендуйте яхты, гидроциклы, лодки в любое время и в любом месте по всему миру. Живите роскошной жизнью своей мечты. Лучшие услуги и мероприятия по лучшим ценам на Oceancraft | ' +
+						'요트, 제트스키, 보트를 언제 어디서든 전 세계에서 구매하고 렌트하세요. 꿈꿔왔던 럭셔리한 삶을 누려보세요. Oceancraft에서 최고의 서비스와 이벤트를 최저가로 만나보세요. | ' +
+						'Xalqaro miqyosda istalgan vaqtda istalgan joyda yaxtalar, jetskilar, qayiqlarni sotib oling va ijaraga oling. Hashamatli orzuingizdagi hayotda yashang. Oceancraft saytida eng yaxshi narxlarda eng yaxshi xizmatlar va tadbirlar.'
 					}
 				/>
 				<meta property="og:image" content="https://ocean-marinecraft.com/img/logo/logo.png" />
