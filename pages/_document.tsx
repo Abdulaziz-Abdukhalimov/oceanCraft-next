e3b4292df59cfa6ddde5a86fb728ce918ea815dd
Abdulaziz Abdukhalimov <abdukhalimov_front_end@Abdulazizs-MacBook-Pro.local>
2026-03-03 13:40:35 +0900

    fix: modify description metada

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,17 +8,28 @@ export default function Document() {
 				<link rel="icon" type="image/png" href="/img/logo/logo.png" />
 
 				{/* SEO */}
-				<meta name="keyword" content={'yacht, yachttour, jetski, mern, mern nestjs fullstack'} />
 				<meta
-					name={'description'}
-					content={
-						'Buy and rent yachts, jetskis, boats anywhere anytime Internationally. Live in your luxury dream life. Best Services and Events at Best prices on Oceancraft | ' +
-						'Покупайте и арендуйте яхты, гидроциклы, лодки в любое время и в любом месте по всему миру. Живите роскошной жизнью своей мечты. Лучшие услуги и мероприятия по лучшим ценам на Oceancraft | ' +
-						'요트, 제트스키, 보트를 언제 어디서든 전 세계에서 구매하고 렌트하세요. 꿈꿔왔던 럭셔리한 삶을 누려보세요. Oceancraft에서 최고의 서비스와 이벤트를 최저가로 만나보세요. | ' +
-						'Xalqaro miqyosda istalgan vaqtda istalgan joyda yaxtalar, jetskilar, qayiqlarni sotib oling va ijaraga oling. Hashamatli orzuingizdagi hayotda yashang. Oceancraft saytida eng yaxshi narxlarda eng yaxshi xizmatlar va tadbirlar.'
-					}
+					name="description"
+					content="Buy and sell or rent yachts, jetskis, boats worldwide. Live your luxury dream life with Oceancraft."
 				/>
+
+				{/* Open Graph (Facebook, WhatsApp, LinkedIn) */}
+				<meta property="og:type" content="website" />
+				<meta property="og:title" content="Oceancraft | Luxury Yachts & Jetski Rentals" />
+				<meta
+					property="og:description"
+					content="Buy and rent yachts, jetskis, boats anywhere anytime internationally."
+				/>
+				<meta property="og:url" content="https://ocean-marinecraft.com/" />
 				<meta property="og:image" content="https://ocean-marinecraft.com/img/logo/logo.png" />
+				<meta property="og:image:width" content="1200" />
+				<meta property="og:image:height" content="630" />
+
+				{/* Twitter */}
+				<meta name="twitter:card" content="summary_large_image" />
+				<meta name="twitter:title" content="Oceancraft | Luxury Yacht Rentals" />
+				<meta name="twitter:description" content="Best yacht and jetski rental services worldwide." />
+				<meta name="twitter:image" content="https://ocean-marinecraft.com/img/logo/logo.png" />
 			</Head>
 			<body>
 				<Main />
